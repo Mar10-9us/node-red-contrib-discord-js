77945c0964860ced67dd65830abeaf114a05b390
Johan Greiner <johan.greiner@outlook.com>
2021-09-18 12:11:17 +0200

    sendSimpleMessage now supports embeds. This will be the main node for sending messages from now on.

--- module/send-simple-message.js
+++ module/send-simple-message.js
@@ -7,7 +7,7 @@ module.exports = function (RED) {
         // Messages gets deleted if the endmost node of the flow is a reply node, or after 15 seconds.
         var connection = RED.nodes.getNode(config.connection);
         // Exit early if no configuration node is specified.
-        if(!(connection)) return;
+        if (!(connection)) return;
 
         var node = this
         node.messageStore = connection.messageStore
@@ -15,71 +15,90 @@ module.exports = function (RED) {
         node.client = connection.client
 
         node.on('input', async function (msg, send, done) {
-
-            msg.payload = msg?.payload || config?.message || null
-            node.action = msg?.action || config?.action || 'send'
+            // This node should replace every-send node
+            console.log('received message')
+            let action = (msg?.action || config?.action || 'send').toLowerCase();
+            let payload = msg?.payload || config?.message | null
             let channelId = msg?.channelId || config.channelId || msg?.discord?.channelId || null
             let messageId = msg?.messageId || msg?.discord?.messageId || null
             let message = messageId ? node.messageStore.messages.get(messageId) : null
+            let isEmbed = msg?.embed === true ? true : false;
+            let messageObj = isEmbed ? { embed: msg.payload } : msg.payload;
 
 
-            if (typeof msg.payload === 'string') {
-                if (!(msg.payload)) {
-                    return done('Cannot send empty string')
-                }
-            } else if (typeof msg.payload === 'number') {
-                msg.payload = String(msg.payload);
-            } else {
-                return done('msg.payload needs to be a string.')
-            }
+            if (isEmbed && typeof payload !== 'object') return done(`To send an embed message, the payload needs to be of type 'object'. Current payload is of type ${typeof payload}`)
+            if (!(isEmbed) && typeof payload !== 'string' && typeof payload !== 'number') return done(`To send a message, the payload needs to be of type 'string' or 'number'. Current payload is of type ${typeof payload}`)
 
 
-            if (node.action === 'send') {
+
+            if (action === 'send') {
+                console.log('in send')
                 if (message) {
-                    message.channel.send(msg.payload)
-                        .then(node.messageStore.delayedDelete(msg.discord.messageId, 15000))
-                        .catch(error => node.warn(error))
+                    try {
+                        message.channel.send(messageObj)
+                        node.messageStore.delayedDelete(msg.discord.messageId, 15000)
+                    } catch (e) {
+                        return done(`Failed to send message: ${e.message}`)
+                    }
+
+
                 } else {
                     if (!(channelId)) return done('No channelId specified')
                     try {
                         let channel = await node.client.channels.fetch(channelId)
                         try {
-                            channel.send(msg.payload)
+                            channel.send(messageObj)
+
                         } catch (e) {
-                            return done('Could not send message')
+                            return done(`Failed to send message: ${e.message}`)
                         }
+
                     } catch (e) {
-                        return done('Could not fetch message')
+                        return done(`Failed to fetch channel: ${e.message}`); // 'Could not fetch channel'
                     }
                 }
 
 
-            } else {
+            } else if (action === 'reply') {
                 // reply
+                console.log('in reply')
                 if (message) {
                     try {
-                        (node.messageStore.messages.get(messageId)).reply(msg.payload)
+                        (node.messageStore.messages.get(messageId)).reply(messageObj)
                             .then(node.messageStore.delayedDelete(messageId, 15000))
                     } catch (e) {
-                        return done('Could not send message')
+                        return done(`Failed to send message: ${e.message}`);
                     }
                 } else {
-                    if (!(messageId)) return done('No messageId specified.')
-                    if (!(channelId)) return done('No channelId specified')
+                    if (!(messageId)) return done('No messageId specified. Specify an ID of which message you want to reply to.');
+                    if (!(channelId)) return done('No channelId specified. You need to specify a channelId to reply to a channel message.');
                     try {
-                        var channel = await node.client.channels.fetch(channelId)
-                        var channelMsg = await channel.messages.fetch(messageId)
+                        var channel = await node.client.channels.fetch(channelId);
+                        var channelMsg = await channel.messages.fetch(messageId);
                     } catch (e) {
-                        return done('Failed to fetch channel or message.')
+                        return done(`Failed to send or fetch message: ${e.message}`);
                     }
-                    
+
                     try {
-                        channelMsg.reply(msg.payload)
+                        channelMsg.reply(messageObj)
                     } catch (e) {
-                        return done('Could not send message')
+                        return done(`Failed to send message: ${e.message}`);
                     }
                 }
+            } else if (action === 'edit') {
+                if (!(channelId)) return done('You need to specify a channelId when attempting to edit a message');
+                if (!(messageId)) return done('You need to specify a messageId when attemtping to edit a message');
+
+                try {
+                    var channels = await node.client.channels.fetch(channelId);
+                    message = await channels.messages.fetch(messageId)
+                    message.edit(messageObj)
+                } catch (e) {
+                    return done(`Failed to edit message: ${e.message}`);
+                }
             }
+
+
 
 
             if (done) done();
